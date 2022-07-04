a38b61581f1e7a12acae71fc715488870cc461a3
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-07-04 14:22:28 -0400

    completed updates to UI

--- src/item.js
+++ src/item.js
@@ -1,7 +1,7 @@
 
 //factory function for creating objects for todo items
 const Task = (title, description, dueDate, priority, project, complete = false) => {
-
+    //create a unique id for each task by making each new task's id 1 more than the highest number id
     const getUniqueId = () => {
        let otherIds = project.items.map(e => Number(e.id.match(/\d{3}$/)));
        if (!otherIds.length) {
